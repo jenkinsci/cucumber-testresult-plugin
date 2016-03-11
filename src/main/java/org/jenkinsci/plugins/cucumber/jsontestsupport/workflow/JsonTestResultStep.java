2c81448e1c77684efafcfd26d0154fb38704a489
Haoyu Wang <wanghaoy@us.ibm.com>
2016-03-10 19:20:16 -0500

    init workflow feature

---
+++ src/main/java/org/jenkinsci/plugins/cucumber/jsontestsupport/workflow/JsonTestResultStep.java
@@ -0,0 +1,10 @@
+package org.jenkinsci.plugins.cucumber.jsontestsupport.workflow;
+
+import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
+
+/**
+ * Created by haoyuwang on 3/10/16.
+ */
+public class JsonTestResultStep extends AbstractStepImpl {
+
+}
